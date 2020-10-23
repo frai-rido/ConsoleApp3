679ecfe6c2b2c914df15d175853c866eafa30b4b
Екатерина Чистова <rido.frai@mail.ru>
2020-10-23 13:03:25 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите ваше имя ");
+            var name = Console.ReadLine();
+            Console.Write("Введите ваш возраст ");
+            var age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ваше имя: {0}, ваш возраст: {1}", name, age);
+            Console.Write("Введите дату вашего рождения ");
+            var birthdate = Console.ReadLine();
+            Console.WriteLine("Дата вашего рождения: {0}", birthdate);
+            
+
+
+            Console.ReadKey();
+
+
+
+            
+
+
+            
+
+
+        }
+    }
+}
